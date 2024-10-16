fd438918d82f175e69836f1674686dfe6b0a91f6
Tauan Neres <tauanspider@gmail.com>
2024-10-16 12:30:23 -0300

    update service

--- src/project/turma/turma.service.ts
+++ src/project/turma/turma.service.ts
@@ -49,4 +49,13 @@ export class TurmaService {
 
     return await this.turmaRepository.save(turma);
   }
+
+  async findAllsSemestre() {
+    return await this.semestreRepository.find();
+  }
+
+  async findOneSemestre(id: number) {
+    const semestre = await this.semestreRepository.findOne({ where: { id } });
+    return semestre;
+  }
 }
